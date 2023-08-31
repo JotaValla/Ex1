d81ec8e0750299f0faa6f7b5e959230ed9138896
User <User@Jota>
2023-08-31 14:11:19 -0500

    Cambios interfaz

---
+++ src/main/java/ConexionBD/CConexion.java
@@ -0,0 +1,31 @@
+
+package ConexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class CConexion {
+    
+    Connection conectar = null;
+    String usuario = "sa";
+    String contrasenia = "Parkour27";
+    String bd = "P1SIGCH";
+    String ip = "localhost";
+    String puerto = "1433";
+    
+    public Connection establecerConexion(){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String cadenaConexion = "jdbc:sqlserver://"+ip+":"+puerto+";"+"databaseName="+bd+";"
+                                                    +"encrypt=true;trustServerCertificate=true";
+            conectar = DriverManager.getConnection(cadenaConexion,usuario,contrasenia);
+            JOptionPane.showMessageDialog(null, "INGRESO CORRECTO A EL SISTEMA", "LOGIN", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+           JOptionPane.showMessageDialog(null, "No se conecto correctamente a la base de datos");
+        }
+        return conectar;
+    }
+    
+}
